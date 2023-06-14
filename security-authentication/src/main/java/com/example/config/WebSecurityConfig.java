33672babb026ac854fe5c4cf72ff57b9a1e9d6c2
-help <jianping.yu@karakal.com.cn>
2023-06-14 16:28:29 +0800

    代码问题提交

--- security-authentication/src/main/java/com/example/config/WebSecurityConfig.java
+++ security-authentication/src/main/java/com/example/config/WebSecurityConfig.java
@@ -1,20 +1,13 @@
 package com.example.config;
 
 import com.example.handler.LogoutSuccessHandler;
-import com.example.service.SpringDataUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import javax.annotation.Resource;
 
 /**
  * @author jianping.yu@karakal.com.cn
